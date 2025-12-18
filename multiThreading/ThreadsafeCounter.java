637709fd4a815d28721083524df636b9e5976d1d
zmallik <94361156+zmallik@users.noreply.github.com>
2025-12-18 20:45:06 +0530

    Update ThreadsafeCounter.java

--- multiThreading/ThreadsafeCounter.java
+++ multiThreading/ThreadsafeCounter.java
@@ -1,1 +1,26 @@
+public class ThreadsExample implements Runnable {
+     static AtomicInteger counter = new AtomicInteger(1); // a global counter
 
+     public ThreadsExample() {
+     }
+
+     static void incrementCounter() {
+          System.out.println(Thread.currentThread().getName() + ": " + counter.getAndIncrement());
+     }
+
+     @Override
+     public void run() {
+          while(counter.get() < 1000){
+               incrementCounter();
+          }
+     }
+
+     public static void main(String[] args) {
+          ThreadsExample te = new ThreadsExample();
+          Thread thread1 = new Thread(te);
+          Thread thread2 = new Thread(te);
+
+          thread1.start();
+          thread2.start();          
+     }
+}
